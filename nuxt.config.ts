8d2f21692807894d84ec3128a4c8bb93f8d08b38
Paweł Sobczak <github@fixeq.qzz.io>
2025-12-06 20:15:05 +0100

    chore: disable source maps and devtools in production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  
+  devtools: { enabled: false },
+
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/icon'
   ],
+
+  sourcemap: false,
 
   app: {
     baseURL: '/',
