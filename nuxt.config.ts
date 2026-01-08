c13cbbd3a4a554a71decfa28b3dc7b4643ae15e8
Paweł Sobczak <github@fixeq.qzz.io>
2026-01-08 04:40:07 +0100

    Set dev server host to 0.0.0.0 and port to 3001

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: false },
+	devServer: {
+		host: '0.0.0.0',
+		port: 3001,
+	},
 
 	modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
 
